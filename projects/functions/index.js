4f5766ca7f2b8f6f7327e3aec1b5ed89e8f4c8d5
Goldobin Vladimir <arue11@mail.ru>
2024-10-15 01:21:16 +0600

    my answers to the week-1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,8 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument(huy) {
-  return huy;
+function returnFirstArgument(ex) {
+  return ex;
 }
 
 
